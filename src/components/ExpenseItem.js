91da8d67c25832668cf6f4b399cd1a5257da4b0e
Chetan-B-B <chetanchetu9740@gmial.com>
2021-12-18 23:29:39 +0530

    18-12-2021

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,16 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  const date = new Date(2021, 11, 18);
+  const title = `Industrial vist`;
+  const amount = 1500;
+  return (
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
