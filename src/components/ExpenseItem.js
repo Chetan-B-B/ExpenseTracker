1fc9324760cb92dfc6229c12433e81e3bf9506ac
Chetan-B-B <chetanchetu9740@gmial.com>
2021-12-20 09:55:35 +0530

    19-12-2021

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-  const date = new Date(2021, 11, 18);
-  const title = `Industrial vist`;
-  const amount = 1500;
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{date.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseItem;
