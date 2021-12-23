2e422dd2a0f5632d1e1a5b183e3a0f6083363d3b
Chetan-B-B <chetanchetu9740@gmial.com>
2021-12-23 23:11:10 +0530

    23-12-2021

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,14 @@
-import { useState } from "react";
-
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Dev chetu!🚀" + Math.random() * 20);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,36 +6,39 @@ import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 function Expenses(props) {
-  const [filteredyear, setYear] = useState("2019");
+  const [filteredyear, setYear] = useState(new Date().getFullYear().toString());
   const filterChangeHandler = (year) => {
     setYear(year);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredyear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredyear}
         onFilterData={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+
+      {filteredExpenses.length === 0 ? (
+        <p className="expenses-list__fallback">
+          What did you do with all of the cash?👀👀
+        </p>
+      ) : (
+        filteredExpenses.map((expense) => {
+          if (+filteredyear === expense.date.getFullYear()) {
+            return (
+              <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+            );
+          }
+        })
+      )}
     </Card>
   );
 }
